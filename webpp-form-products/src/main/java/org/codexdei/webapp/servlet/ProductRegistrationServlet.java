3261c4f066e099757a9f1d81da0ec15343b65a2c
codexdei <jlacostc@gmail.com>
2025-06-12 10:19:43 -0500

    se crea clase ProductRegistrationServlet con sus respectivos metodos doGet y doPost

---
+++ webpp-form-products/src/main/java/org/codexdei/webapp/servlet/ProductRegistrationServlet.java
@@ -0,0 +1,49 @@
+package org.codexdei.webapp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/create")
+public class ProductRegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String nameProduct = req.getParameter("nameproduct");
+        int priceProduct = Integer.parseInt("priceproduct");
+        String manufacture = req.getParameter("manufacture");
+
+        //Para validacion de errores
+        Map<String,String> errors = new HashMap<>();
+
+        resp.setContentType("text/html");
+
+        try (PrintWriter out = resp.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset=\"UTF-8\">");
+            out.println("       <title></title>");
+            out.println("   </head>");
+            out.println("   <body>");
+            out.println("       <h1></h1>");
+            out.println("   </body>");
+            out.println("</html>");
+        }
+    }
+}
